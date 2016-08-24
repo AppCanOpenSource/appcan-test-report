864c093cb593e95ac02fcf403a545e567b80b36c
yltwust <longtao.yan@zymobi.com>
2016-08-24 15:47:03 +0800

    解决case不能跑的问题

--- pluginTestcase/case/js/uexXMLHttpMgr.js
+++ pluginTestcase/case/js/uexXMLHttpMgr.js
@@ -3,7 +3,7 @@
  */
 if (UNIT_TEST) {
 
-  UNIT_TEST.log("###########先启动服务器，更新请求的URL地址。###########");
+  //UNIT_TEST.log("###########先启动服务器，更新请求的URL地址。###########");
 
 	var uexXMLHttpMgrCase = {
 		"testPostInAppcanJS" : function() {
