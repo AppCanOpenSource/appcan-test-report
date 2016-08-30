a8564a016bbe8565c4588490c24e813c77455e6a
fred <fei.ye@3g2win.com>
2016-08-30 15:16:41 +0800

    提交uexCreditCardRec自动化测试case

---
+++ pluginTestcase/case/js/uexCreditCardRec.js
@@ -0,0 +1,17 @@
+if (UNIT_TEST) {
+    var uexCreditCardRecCase = {
+        "case": function(){
+            var tokenStr = "f06a7eca39134918a18dc4d7c45ee49f";
+            //测试卡信息可以用 http://kaku.51credit.com/bocomm/jhbiaozhun/
+            uexCreditCardRec.openCreditCardRec(tokenStr, function(error,data){
+                if(!error){
+                    UNIT_TEST.log("[data]" + data.cardNumber);
+                    UNIT_TEST.assert(true);
+                }else{
+                   UNIT_TEST.assert(false);
+                 }
+            });
+        }
+    };
+    UNIT_TEST.addCase("creditCardRec", uexCreditCardRecCase);
+}
