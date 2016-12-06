e1658e0ff6d515cd0371fa45342298cfe628f06c
ylt <ylt@ylt-MacBook-Pro.local>
2016-12-06 15:51:56 +0800

    支持读取APP预置数据库

--- pluginTestcase/case/js/uexDatabaseMgr.js
+++ pluginTestcase/case/js/uexDatabaseMgr.js
@@ -90,6 +90,30 @@ if (UNIT_TEST) {
             }else{
                 UNIT_TEST.assert(false);
             }
+        },
+        "readCustomDb":function () {
+            uexDataBaseMgr.copyDataBaseFile("res://musicEx.db",function (error) {
+                if (!error) {
+
+                    var db = uexDataBaseMgr.open("musicEx.db", 1017);
+                    if (db != null) {
+                        var sql = "SELECT * FROM Song";
+                        uexDataBaseMgr.select(db, sql, function (error, data) {
+                            if (error) {
+                                UNIT_TEST.assert(false);
+                            } else {
+                                UNIT_TEST.log(JSON.stringify(data));
+                                UNIT_TEST.assert(true);
+                            }
+                            db.close();
+                        });
+                    } else {
+                        UNIT_TEST.assert(false);
+                    }
+                }else{
+                    UNIT_TEST.assert(false);
+                }
+            });
         }
 
     }
