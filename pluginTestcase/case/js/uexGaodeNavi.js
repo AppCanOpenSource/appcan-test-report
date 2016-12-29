8fcd3c35d0e4b73d08e22462f244b35ced3add4a
connyliu <21165491@qq.com>
2016-12-29 09:26:30 +0800

    更新高德导航case

--- pluginTestcase/case/js/uexGaodeNavi.js
+++ pluginTestcase/case/js/uexGaodeNavi.js
@@ -1,65 +1,163 @@
-if (UNIT_TEST) {
-    var uexGaodeCase = {
-        "init":function(){
-            uexGaodeNavi.init(function(error,data){
-                              if(!error){
-                              UNIT_TEST.assert(true);
-                              }else{
-                              UNIT_TEST.assert(false);
-                              }
-                              });
-        },
+define(["CC","Rx"],function(CC,Rx){
 
-        "onGetNavigationText":function () {
-            uexGaodeNavi.onGetNavigationText = function(obj){
-                    UNIT_TEST.log(obj);
-                
-            }
-            UNIT_TEST.assert(true);
-        },
-        "calculateDriveRoute":function () {
-            var params = {
-            startPoint:[39.925846, 116.432765],
-            wayPoints:[[39.925846, 116.432345],[39.925846, 116.432789]],
-            endPoint:[39.925041, 116.437901]
-            };
-            
-                uexGaodeNavi.calculateDriveRoute(params,function(error,data){
-                    UNIT_TEST.assert(!error);
-                });
-          
-            
-        },
-//        "calculateWalkRoute":function () {
-//            var params = {
-//            startPoint:[39.925846, 116.432765],
-//            endPoint:[39.925041, 116.437901]
-//            };
-//            
-//            uexGaodeNavi.calculateWalkRoute(params,function(error,data){
-//                                             UNIT_TEST.assert(!error);
-//                                             });
-//            
-//            
-//        },
-        "startNavi":function () {
-            var params = {
-            type:1
-            };
-            var data = JSON.stringify(params);
-            uexGaodeNavi.startNavi(data);
-            UNIT_TEST.assert(true);
-        },
-        "stopNavi":function(){
-            setTimeout(function(){
-                       uexGaodeNavi.stopNavi();
-                    },5000)
-            
-            
-            UNIT_TEST.assert(true);
+  if (!UNIT_TEST){return;};
+  
+  const EMPTY_FUNC = function(){};
+  const ERROR_HANDLER = function(err){
+    CC.log(err);
+    uexGaodeNavi.stopNavi();
+    UNIT_TEST.assert(false);
+  };
+  const SUCCESS_HANDLER = function(){UNIT_TEST.assert(true);};
+
+  var TEST_CASE = {};
+
+  var alertSignal = function(msg){
+    return Rx.Observable.create(function (observer) {
+      CC.alert(msg,function(){observer.onCompleted();});
+    });
+  };
+  var walkRouteSignal = function(){
+    return Rx.Observable.create(function (observer) {
+      var params = {
+        startPoint:[39.925846, 116.432765],
+        endPoint:[39.925041, 116.437901]
+      };
+      uexGaodeNavi.calculateWalkRoute(JSON.stringify(params),function(err){
+        if (err ===  0) {
+          observer.onCompleted();
+        }else{
+          observer.onError(new Error("步行路径规划失败!"));
         }
-        
-    };
-    
-    UNIT_TEST.addCase("Gaode", uexGaodeCase);
-}
+      });
+    });
+  };
+  var driveRouteSignal = function(){
+    return Rx.Observable.create(function (observer){
+      var params = {
+        startPoint:[39.925846, 116.432765],
+        endPoint:[39.925041, 116.437901]
+      };
+      uexGaodeNavi.calculateDriveRoute(JSON.stringify(params),function(err){
+        if (err ===  0) {
+          observer.onCompleted();
+        }else{
+          observer.onError(new Error("驾车路径规划失败!"));
+        }
+      });
+    })
+  };
+  var startNaviSignal = function(){
+    return Rx.Observable
+      .create(function (observer) {
+        var params = {type: 1};
+        uexGaodeNavi.onStartNavi = function(){
+            CC.log("startNavi!!!");
+          observer.onCompleted();
+        };
+        uexGaodeNavi.startNavi(JSON.stringify(params));
+      })
+      .timeout(5000,new Error("操作超时!"))
+      .finally(function(){
+        uexGaodeNavi.onStartNavi = null;
+      });
+  };
+  var observeNaviSignal = function(){
+    return Rx.Observable
+      .create(function(observer){
+        var parseNaviText = function (info){
+          var data = JSON.parse(info);
+          switch(data.type){
+            case 2:
+              return "前方路况 ~> " + data.text;
+            case 4:
+              return "周边路况 ~> " + data.text;
+            case 8:
+              return "通过提示 ~> " + data.text;
+            default:
+              return "导航播报 ~> " + data.text;
+          }
+        }
+        uexGaodeNavi.onGetNavigationText = function(info){
+          CC.log(parseNaviText(info));
+        };
+        uexGaodeNavi.onReCalculateRouteForYaw = function(){
+          CC.log("出现偏航,重新计算路径...");
+        };
+        uexGaodeNavi.onArriveDestination = function(){
+          observer.onCompleted();
+          CC.log("到达目的地!");
+        };
+        uexGaodeNavi.onNaviCancel = function(){
+          CC.log("用户关闭了导航界面!");
+          observer.onCompleted();
+        }
+      })
+      .delay(1500)//等待导航界面关闭
+      .finally(function(){
+        uexGaodeNavi.onGetNavigationText = null;
+        uexGaodeNavi.onReCalculateRouteForYaw = null;
+        uexGaodeNavi.onArriveDestination = null;
+        uexGaodeNavi.onNaviCancel = null;
+      });
+  };
+
+
+  TEST_CASE.init = function(){
+
+    uexGaodeNavi.init(null,function (err){
+      var ret = (err === 0) ;
+      CC.log("高德导航初始化结果: " + ret ? "成功" : "失败");
+      UNIT_TEST.assertTrue(ret);
+    });
+  };
+  TEST_CASE.walkNavi = function(){
+    alertSignal("开始测试步行导航.")
+      .concat(walkRouteSignal())
+      .concat(alertSignal("步行路径规划成功,即将打开导航页面..."))
+      .concat(Rx.Observable.defer(startNaviSignal))
+      .concat(Rx.Observable.defer(observeNaviSignal))
+      .subscribe(
+        EMPTY_FUNC,
+        ERROR_HANDLER,
+        SUCCESS_HANDLER
+      );
+  };
+  TEST_CASE.driveNavi = function(){
+    alertSignal("开始测试驾车导航.")
+      .concat(driveRouteSignal())
+      .concat(alertSignal("驾车路径规划成功,即将打开导航页面..."))
+      .concat(Rx.Observable.defer(startNaviSignal))
+      .concat(Rx.Observable.defer(observeNaviSignal))
+      .subscribe(
+        EMPTY_FUNC,
+        ERROR_HANDLER,
+        SUCCESS_HANDLER
+      );
+  };
+  TEST_CASE.stopNavi = function(){
+    alertSignal("开始测试stopNavi接口.")
+      .concat(walkRouteSignal())
+      .concat(alertSignal("打开导航页面后3秒将调用接口关闭导航页面."))
+      .concat(Rx.Observable.defer(startNaviSignal))
+      .delay(3000)
+      .subscribe(
+        EMPTY_FUNC,
+        function(err){
+          CC.log("ERROR: " + err);
+          UNIT_TEST.assert(false);
+        },
+        function(){
+          uexGaodeNavi.stopNavi();
+          CC.confirm("导航页面被正确关闭了吗?",function(ret){
+            UNIT_TEST.assert(ret);
+          });
+        }
+      );
+  };
+
+
+  UNIT_TEST.addCase("uexGaodeNavi", TEST_CASE);
+
+
+});
