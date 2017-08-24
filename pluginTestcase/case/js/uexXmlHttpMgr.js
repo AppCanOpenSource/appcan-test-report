74ce547375219c124a34d1f1a244a207338880aa
CloudWind <leegq827@163.com>
2017-08-24 17:21:13 +0800

    update host
    update host

--- pluginTestcase/case/js/uexXmlHttpMgr.js
+++ pluginTestcase/case/js/uexXmlHttpMgr.js
@@ -24,7 +24,7 @@ if (UNIT_TEST) {
 
 //		    var reqUrl = "http://192.168.1.47:1337/testPost";
 
-		    var reqUrl = "http://139.224.11.153:45678/post";
+		    var reqUrl = "http://120.76.190.76:45678/post";
 
 			testPostWithData(reqUrl);
 		},
@@ -32,7 +32,7 @@ if (UNIT_TEST) {
 
 //		    var reqUrl = "http://192.168.1.47:1337/testPostBody";
 
-		    var reqUrl = "http://139.224.11.153:45678/post";
+		    var reqUrl = "http://120.76.190.76:45678/post";
 
 			testPostWithBody(reqUrl);
 		},
@@ -40,7 +40,7 @@ if (UNIT_TEST) {
 
 //		    var reqUrl = "http://192.168.1.47:1337/testPostStream";
 
-            var reqUrl = "http://139.224.11.153:45678/post";
+            var reqUrl = "http://120.76.190.76:45678/post";
 
 			testPostWithStream(reqUrl);
 		},
@@ -48,7 +48,7 @@ if (UNIT_TEST) {
 
 //		    var reqUrl = "http://192.168.1.47:1337";
 
-		    var reqUrl = "http://139.224.11.153:45678/post";
+		    var reqUrl = "http://120.76.190.76:45678/post";
 
 			testCookie(reqUrl);
 		},
